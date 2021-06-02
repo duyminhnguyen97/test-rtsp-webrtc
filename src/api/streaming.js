2cb19e14ae6ec66428feba32066958f98fd2e1a5
duyminhnguyen97 <ndminh1997@gmail.com>
2021-06-02 14:50:05 +0700

    test buffer array

--- src/api/streaming.js
+++ src/api/streaming.js
@@ -1,9 +1,9 @@
 const ffmpeg = require('fluent-ffmpeg');
-const wrtc = require('wrtc');
-const w2f = require('wrtc-to-ffmpeg')(wrtc);
 
 const ffmpegPath = 'F:\\Huggy-electron\\ffmpeg\\bin\\ffmpeg.exe';
+const ffprobe = 'F:\\Huggy-electron\\ffmpeg\\bin\\ffprobe.exe';
 ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobe);
 
 let cameraUrl = '';
 
@@ -19,29 +19,53 @@ const addRTSPUrl = async (event) => {
 };
 
 const recordCamera = async () => {
+    // rtsp://admin:BKPJKM@192.168.0.100
     // WORK HERE
     console.log('OK');
 
     try {
-        const videoOutput = await w2f.output({ kind: 'video', width: 480, height: 360 });
-        const audioOutput = await w2f.output({ kind: 'audio', sampleRate: 48000 });
+        const command = ffmpeg()
+            .input('rtsp://admin:BKPJKM@192.168.0.100')
+            .noAudio()
+            .fps(10)
+            .format('flv')
+            .videoCodec('libx264')
+            .on('error', (err, stdout, stderr) => {
+                console.error(err.message);
+                console.log(stdout);
+                console.log(stderr);
+            })
+            .on('end', () => {
+                console.warning(`Camera ended`);
+            });
 
-        console.log(videoOutput);
-        console.log(videoOutput.track);
+        let streamElement = document.getElementById('stream');
+        const mediaSource = new MediaSource();
+        streamElement.src = URL.createObjectURL(mediaSource);
+        streamElement.play();
+        let sourceBuffer = null;
+        console.log(streamElement);
 
-        const stream = document.querySelector('#stream');
+        mediaSource.addEventListener('sourceopen', () => {
+            console.log('Source open');
+            sourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="avc1.64001F"');
+        });
 
-        // ffmpeg('rtsp://admin:BKPJKM@192.168.0.107')
-        //     .output(videoOutput.url)
-        //     .outputOptions(videoOutput.options)
-        //     .output(audioOutput.url)
-        //     .outputOptions(audioOutput.options);
+        const stream = command.pipe();
+        stream.on('data', (chunk) => {
+            if (mediaSource.readyState === 'open' && sourceBuffer && sourceBuffer.updating === false) {
+                console.log(chunk.buffer);
+                sourceBuffer.appendBuffer(chunk.buffer);
+                console.log(sourceBuffer);
+                console.log(mediaSource.duration);
+            }
+            // let blobArray = [];
+            // blobArray.push(new Blob([new Uint8Array(chunk)], { type: 'video/mp4' }));
+            // const blob = new Blob(blobArray, { type: 'video/mp4' });
+            // console.log(blob);
 
-        // const video = new MediaStream();
-        // console.log(videoOutput.track);
-        // video.addTrack(videoOutput.track);
-
-        // console.log(video.captureStream());
+            // streamElement.src = URL.createObjectURL(blob);
+        });
     } catch (error) {
         console.log(error);
     }
